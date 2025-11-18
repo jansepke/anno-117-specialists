09cdad8077541d3d54b74c7671c75a015f4c694c
Jan Sepke <625043+jansepke@users.noreply.github.com>
2025-11-18 22:09:42 +0100

    add translation switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import type React from "react";
-import items from "./db/en/item.json" with { type: "json" };
-import assetpools from "./db/en/assetpoolnamed.json" with { type: "json" };
+import itemsEN from "./db/en/item.json" with { type: "json" };
+import assetpoolsEN from "./db/en/assetpoolnamed.json" with { type: "json" };
+import itemsDE from "./db/de/item.json" with { type: "json" };
+import assetpoolsDE from "./db/de/assetpoolnamed.json" with { type: "json" };
+import { useTranslation } from "react-i18next";
 
 export const App: React.FC = () => {
-  return items.map((item) => (
-    <div key={item.id}>
-      {item.name} ({item.rarity}) on {assetpools.find((pool) => pool.id === item.target)?.name}
-    </div>
-  ));
+  const { i18n } = useTranslation();
+
+  const items = i18n.language === "de" ? itemsDE : itemsEN;
+  const assetpools = i18n.language === "de" ? assetpoolsDE : assetpoolsEN;
+
+  return (
+    <>
+      <button onClick={() => i18n.changeLanguage("en")}>en</button>
+      <button onClick={() => i18n.changeLanguage("de")}>de</button>
+      {items.map((item) => (
+        <div key={item.id}>
+          {item.name} ({item.rarity}) on {assetpools.find((pool) => pool.id === item.target)?.name}
+        </div>
+      ))}
+    </>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
+import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
 
-createRoot(document.getElementById('root')!).render(
+i18next
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: ["en", "de"],
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
