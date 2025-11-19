c5ed1c8bc2acd689a4e007b037c47f3369c37125
Jan Sepke <625043+jansepke@users.noreply.github.com>
2025-11-19 22:37:14 +0100

    remove unused code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,6 @@
 import type React from "react";
 import { useTranslation } from "react-i18next";
-import assetpoolsDE from "../db/de/assetpoolnamed.json" with { type: "json" };
 import itemsDE from "../db/de/item.json" with { type: "json" };
-import assetpoolsEN from "../db/en/assetpoolnamed.json" with { type: "json" };
 import itemsEN from "../db/en/item.json" with { type: "json" };
 import type { AnnoItem } from "../types";
 import { ItemList } from "./ItemList";
@@ -12,7 +10,6 @@ export const App: React.FC = () => {
   const { i18n } = useTranslation();
 
   const items = i18n.language === "de" ? itemsDE : itemsEN;
-  const assetpools = i18n.language === "de" ? assetpoolsDE : assetpoolsEN;
 
   return (
     <>
